ad07644d821de50a5b3971b32ff8a8976231f704
InterfectoremCubiculum <133600442+InterfectoremCubiculum@users.noreply.github.com>
2024-11-13 00:48:07 +0100

    Added Comments in file controller and material controller

--- catch-up-backend/Controllers/FileController.cs
+++ catch-up-backend/Controllers/FileController.cs
@@ -14,6 +14,13 @@ namespace catch_up_backend.Controllers
             _fileService = fileService;
         }
 
+        /// <summary>
+        /// It save file into the storage
+        /// and make relation with material
+        /// </summary>
+        /// <param name="file">File to upload</param>
+        /// <param name="materialId"></param>
+        /// <returns> fileDto and the same materialID</returns>
         [HttpPost]
         [Route("Upload")]
         public async Task<IActionResult> Upload(IFormFile file, int? materialId)
@@ -22,6 +29,12 @@ namespace catch_up_backend.Controllers
             return Ok(new { message = "File uploaded ", fileDto, materialId });
         }
 
+        /// <summary>
+        /// It physically delete file in storage, it not remove file record in database.
+        /// File record change state to Deleted
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
         [HttpDelete]
         [Route("Delete/{fileId:int}")]
         public async Task<IActionResult> Delete(int fileId)
@@ -30,6 +43,11 @@ namespace catch_up_backend.Controllers
             return Ok(new { message = "File deleted"});
         }
 
+        /// <summary>
+        /// It change state of file in database to Archived
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
         [HttpPatch]
         [Route("Archive/{fileId:int}")]
         public async Task<IActionResult> Archive(int fileId)
@@ -38,6 +56,11 @@ namespace catch_up_backend.Controllers
             return Ok(new { message = "File archived" });
         }
 
+        /// <summary>
+        /// Use to get fileDto by fileId (only in Active state)
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("Get/{fileId:int}")]
         public async Task<IActionResult> Get(int fileId)
@@ -46,6 +69,11 @@ namespace catch_up_backend.Controllers
             return Ok(new { message = "File found", fileDto });
         }
 
+        /// <summary>
+        /// Download file by returning stream
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns>Stream with the file saved in storage</returns>
         [HttpGet]
         [Route("Download/{fileId:int}")]
         public async Task<IActionResult> Download(int fileId)
