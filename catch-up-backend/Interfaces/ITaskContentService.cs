262d7471a0f8f9132f7bbd9ba03c6278a8c9322f
Triadziuch <kurowski1234567890@gmail.com>
2024-11-19 22:26:35 +0100

    Adjusted TaskContent Model and Dto to work with frontend interface for adding tasks.

--- catch-up-backend/Interfaces/ITaskContentService.cs
+++ catch-up-backend/Interfaces/ITaskContentService.cs
@@ -10,6 +10,7 @@ namespace catch_up_backend.Interfaces
         public Task<bool> Delete(int taskContentId);
         public Task<List<TaskContentDto>> GetAll();
         public Task<TaskContentDto> GetById(int taskContentId);
+        public Task<List<TaskContentDto>> GetByTitle(string title);
         public Task<List<TaskContentDto>> GetByCreatorId(Guid creatorId);
         public Task<List<TaskContentDto>> GetByCategoryId(int categoryId);
     }
--- catch-up-backend/Models/TaskContentModel.cs
+++ catch-up-backend/Models/TaskContentModel.cs
@@ -9,15 +9,15 @@ namespace catch_up_backend.Models
         [Key]
         public int Id { get; set; }
         [ForeignKey("CreatorId")]
-        public Guid CreatorId { get; set; }
+        public Guid? CreatorId { get; set; }
         [ForeignKey("CategoryId")]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         [ForeignKey("MaterialsId")]
-        public int MaterialsId { get; set; }
+        public int? MaterialsId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public StateEnum State { get; set; }
-        public TaskContentModel(Guid creatorId, int categoryId, int materialsId, string title, string description)
+        public TaskContentModel(Guid? creatorId, int? categoryId, int? materialsId, string title, string description)
         {
             CreatorId = creatorId;
             CategoryId = categoryId;
