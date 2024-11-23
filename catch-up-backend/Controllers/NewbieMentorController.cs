933c5840e8b8c165855b1f0f9f82f2183d52b603
Mateusz Izdebski <mateusz.izdebski125@gmail.com>
2024-11-23 13:13:46 +0100

    com del

--- catch-up-backend/Controllers/NewbieMentorController.cs
+++ catch-up-backend/Controllers/NewbieMentorController.cs
@@ -144,7 +144,7 @@ namespace catch_up_backend.Controllers
 
             return Ok(mentors);
         }
-        // Pobieranie wszystkich nowych pracowników jeszcze nie przypisanych do konretnego mentora. Kot
+        // Pobieranie wszystkich nowych pracowników jeszcze nie przypisanych do konretnego mentora.
         [HttpGet]
         [Route("GetAllUnassignedNewbies/{mentorId:guid}")]
         public async Task<IActionResult> GetAllUnassignedNewbies(Guid mentorId)
