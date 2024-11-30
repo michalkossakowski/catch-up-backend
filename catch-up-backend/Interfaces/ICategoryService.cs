f9227e1777d5e1cb490bf9ac79528322bc16e036
Triadziuch <kurowski1234567890@gmail.com>
2024-11-30 12:17:49 +0100

    Category controller, service and Dto.

---
+++ catch-up-backend/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using catch_up_backend.Dtos;
+
+namespace catch_up_backend.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<bool> AddCategory(CategoryDto newCategory);
+        public Task<bool> EditCategory(int categoryId, CategoryDto newCategory);
+        public Task<bool> DeleteCategory(int categoryId);
+        public Task<List<CategoryDto>> GetAll();
+        public Task<CategoryDto> GetById(int categoryId);
+        public Task<List<CategoryDto>> SearchCategories(string searchingString);
+        public Task<bool> IsUnique(string categoryName);
+        public Task<int> GetCount();
+    }
+}
