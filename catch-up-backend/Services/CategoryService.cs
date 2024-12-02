0606d1976711921aab59da2543520dbd4465583a
ErykSliwowski <eryksliwowskic@gmail.com>
2024-12-02 13:04:58 +0100

    - resolved conflict after merge

--- catch-up-backend/Services/CategoryService.cs
+++ catch-up-backend/Services/CategoryService.cs
@@ -126,5 +126,23 @@ namespace catch_up_backend.Services
                 .Where(c => c.State != StateEnum.Deleted)
                 .CountAsync();
         }
+        public async Task<CategoryDto> GetActiveCategory(int categoryId)
+        {
+            var categoryModel = await _context.Categories.FindAsync(categoryId)
+                ?? throw new NotFoundException("Category not found");
+            if (categoryModel.State != Enums.StateEnum.Active)
+                throw new NotFoundException("Category not is not active");
+            return new CategoryDto { Id = categoryModel.Id, Name = categoryModel.Name };
+        }
+
+        public async Task<bool> IsActive(int categoryId)
+        {
+            return await _context.Categories.AnyAsync(c => c.Id == categoryId && c.State == Enums.StateEnum.Active);
+        }
+
+        public async Task<bool> IsExists(int categoryId)
+        {
+            return await _context.Categories.AnyAsync(c => c.Id == categoryId);
+        }
     }
 }
