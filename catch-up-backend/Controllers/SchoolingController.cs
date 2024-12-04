5750f274f27b5a53b35f7ed843c7c086749e6588
ErykSliwowski <eryksliwowskic@gmail.com>
2024-12-04 01:38:22 +0100

    - assing schooling to user - archive/delete schooling - archive/delete assigned schooling to user - getting all schooling or all schooling assign to user

--- catch-up-backend/Controllers/SchoolingController.cs
+++ catch-up-backend/Controllers/SchoolingController.cs
@@ -20,44 +20,68 @@ namespace catch_up_backend.Controllers
         [Route("Create")]
         public async Task<IActionResult> Create([FromBody] SchoolingDto schoolingDto)
         {
-            var schooling = await _schoolingService.CreateSchoolingAsync(schoolingDto);
+            var fullSchoolingDto = await _schoolingService.CreateSchooling(schoolingDto);
 
-            if (schooling.Schooling.Id == 0)
+            if (fullSchoolingDto.Schooling.Id == 0)
             {
                 throw new Exception("Id not set for the created Schooling");
             }
-            return CreatedAtAction("GetFull", new { schoolingId = schooling.Schooling.Id }, new { message = "Schooling  created", schooling });
+            return CreatedAtAction("GetFull", new { schoolingId = fullSchoolingDto.Schooling.Id }, new { message = "Schooling  created",data = fullSchoolingDto });
         }
 
 
         [HttpGet]
         [Route("GetFull/{schoolingId:int}")]
-        public async Task<IActionResult> GetFullAsync(int schoolingId)
+        public async Task<IActionResult> GetFull(int schoolingId)
         {
-            var fullSchoolingDto = await _schoolingService.GetFullAsync(schoolingId);
+            var fullSchoolingDto = await _schoolingService.GetFull(schoolingId);
             return Ok(new { message = "Schooling found", fullSchoolingDto });
         }
 
         [HttpGet]
         [Route("GetAllFull")]
-        public async Task<IActionResult> GetAllFullAsync()
+        public async Task<IActionResult> GetAllFull()
         {
-            var schoolings = await _schoolingService.GetAllFullAsync();
+            var schoolings = await _schoolingService.GetAllFull();
+            return Ok(new { message = "Schoolings retrieved successfully", data = schoolings });
+        }
+
+        [HttpGet]
+        [Route("GetUserSchoolingsID/{userId:Guid}")]
+        public async Task<IActionResult> GetUserSchoolingsID(Guid userId)
+        {
+            var schoolingsIds = await _schoolingService.GetUserSchoolingsID(userId);
+            return Ok(new { message = "Schoolings retrieved successfully", data = schoolingsIds });
+        }
+
+        [HttpGet]
+        [Route("GetAllFull/{userId:Guid}")]
+        public async Task<IActionResult> GetAllFull(Guid userId)
+        {
+            var schoolings = await _schoolingService.GetAllFull(userId);
             return Ok(new { message = "Schoolings retrieved successfully", data = schoolings });
         }
 
         [HttpDelete]
-        [Route("Delete/{schoolingId:int}")]
-        public async Task<IActionResult> DeleteAsync(int schoolingId)
+        [Route("DeleteSchooling/{schoolingId:int}")]
+        public async Task<IActionResult> DeleteSchooling(int schoolingId)
         {
-            var isDeleted = await _schoolingService.DeleteAsync(schoolingId);
+            await _schoolingService.DeleteSchooling(schoolingId);
             return Ok(new { message = "Schooling deleted successfully" });
+        }
+
+        [HttpDelete]
+        [Route("ArchiveSchooling/{schoolingId:int}")]
+        public async Task<IActionResult> ArchiveSchooling(int schoolingId)
+        {
+            await _schoolingService.ArchiveSchooling(schoolingId);
+            return Ok(new { message = "Schooling archived successfully" });
         }
 
         // Do dokończenia
         [HttpPut]
         [Route("Edit")]
-        public async Task<IActionResult> EditAsync([FromBody] FullSchoolingDto fullSchoolingDto)
+        public async Task<IActionResult> Edit([FromBody] FullSchoolingDto fullSchoolingDto)
         {
             await _schoolingService.Edit(fullSchoolingDto);
             return Ok(new { message = "Schooling updated successfully" });
@@ -65,10 +89,34 @@ namespace catch_up_backend.Controllers
 
         [HttpPost]
         [Route("AddSchoolingPart/{schoolingId:int}")]
-        public async Task<IActionResult> AddSchoolingPartAsync(int schoolingId, [FromBody] SchoolingPartDto schoolingPartDto)
+        public async Task<IActionResult> AddSchoolingPart(int schoolingId, [FromBody] SchoolingPartDto schoolingPartDto)
         {
             await _schoolingService.AddSchoolingPart(schoolingPartDto, schoolingId);
             return Ok(new { message = "Schooling part added successfully" });
+        }
+
+        [HttpPost]
+        [Route("AddSchoolingToUser/{userId:Guid}/{schoolingId:int}")]
+        public async Task<IActionResult> AddSchoolingToUser(Guid userId, int schoolingId)
+        {
+            await _schoolingService.AddSchoolingToUser(userId, schoolingId);
+            return Ok(new { message = "Added schooling to user" });
+        }
+
+        [HttpPost]
+        [Route("ArchiveUserSchooling/{userId:Guid}/{schoolingId:int}")]
+        public async Task<IActionResult> ArchiveUserSchooling(Guid userId, int schoolingId)
+        {
+            await _schoolingService.ArchiveUserSchooling(userId, schoolingId);
+            return Ok(new { message = "Schooling successfully archived" });
+        }
+
+        [HttpPost]
+        [Route("DeleteUserSchooling/{userId:Guid}/{schoolingId:int}")]
+        public async Task<IActionResult> DeleteUserSchooling(Guid userId, int schoolingId)
+        {
+            await _schoolingService.DeleteUserSchooling(userId, schoolingId);
+            return Ok(new { message = "Schooling successfully deleted" });
         }
     }
 }
--- catch-up-backend/Interfaces/ISchoolingService.cs
+++ catch-up-backend/Interfaces/ISchoolingService.cs
@@ -4,11 +4,17 @@ namespace catch_up_backend.Interfaces
 {
     public interface ISchoolingService
     {
-        public Task<FullSchoolingDto> GetFullAsync(int schoolingId);
-        public Task<FullSchoolingDto> CreateSchoolingAsync(SchoolingDto schoolingDto);
-        public Task<List<FullSchoolingDto>> GetAllFullAsync();
+        public Task<FullSchoolingDto> GetFull(int schoolingId);
+        public Task<FullSchoolingDto> CreateSchooling(SchoolingDto schoolingDto);
+        public Task<List<FullSchoolingDto>> GetAllFull();
         public Task Edit(FullSchoolingDto fullSchoolingDto);
         public Task AddSchoolingPart(SchoolingPartDto schoolingPart, int schoolingID);
-        public Task<bool> DeleteAsync(int schoolingId);
+        public Task DeleteSchooling(int schoolingId);
+        public Task ArchiveSchooling(int schoolingId);
+        public Task<List<FullSchoolingDto>> GetAllFull(Guid userId);
+        public Task AddSchoolingToUser(Guid userId, int schoolingId);
+        public Task<List<int>> GetUserSchoolingsID(Guid userId);
+        public Task ArchiveUserSchooling(Guid userId, int schoolingId);
+        public Task DeleteUserSchooling(Guid userId, int schoolingId);
     }
 }
