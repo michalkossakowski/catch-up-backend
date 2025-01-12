284adc277b76670e798e6e658b24dc33f2e0a3b8
Triadziuch <kurowski1234567890@gmail.com>
2025-01-12 16:42:16 +0100

    Testowe dane dla Categories, Presets, TaskCOntents i TaskPresets

---
+++ catch-up-backend/TestData/dbo.Categories.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Categories] ON
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (1, N'Programowanie', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (2, N'Projektowanie', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (3, N'Marketing', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (4, N'Analiza Danych', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (5, N'Testowanie Oprogramowania', 0);
+SET IDENTITY_INSERT [dbo].[Categories] OFF;
