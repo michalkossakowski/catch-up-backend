8ce8a00c2ca8d8f665f4dbe032197df11215efa2
Triadziuch <kurowski1234567890@gmail.com>
2025-01-12 16:14:56 +0100

    Fixed obligatory MaterialId and CategoryId in TaskContent table.

---
+++ catch-up-backend/Migrations/20250112151232_AllowNullableTaskContentReferences.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace catch_up_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullableTaskContentReferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaterialsId",
+                table: "TaskContents",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "TaskContents",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaterialsId",
+                table: "TaskContents",
+                type: "int",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "TaskContents",
+                type: "int",
+                nullable: false);
+        }
+    }
+}
