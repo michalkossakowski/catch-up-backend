292d13f0cc37d0f0fe4b93e76d19d73eb5b54a70
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2025-01-14 21:28:51 +0100

    - changed sample data

--- catch-up-backend/TestData/dbo.Categories.data.sql
+++ catch-up-backend/TestData/dbo.Categories.data.sql
@@ -4,4 +4,11 @@ INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (2, N'Projektowani
 INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (3, N'Marketing', 0);
 INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (4, N'Analiza Danych', 0);
 INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (5, N'Testowanie Oprogramowania', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (6, N'Kursy Online', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (7, N'Szkolenia Zawodowe', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (8, N'Programowanie i Rozwój Oprogramowania', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (9, N'Chmura i Technologie Sieciowe', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (10, N'Zarządzanie i Przedsiębiorczość', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (11, N'Zarządzanie Zasobami Ludzkimi', 0);
+INSERT INTO [dbo].[Categories] ([Id], [Name], [State]) VALUES (12, N'Internet Rzeczy (IoT)', 0);
 SET IDENTITY_INSERT [dbo].[Categories] OFF;
